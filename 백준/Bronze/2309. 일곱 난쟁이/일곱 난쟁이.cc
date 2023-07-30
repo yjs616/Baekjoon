d03ccab8ad4eaa8d226ba916078be6db02584738
YU JEONG SEON <yjs000616@gmail.com>
2023-07-30 20:41:57 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,48 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int sum;
 vector<int> height(9), h;
-
-bool print(vector<int>& h) {
-    int sum = 0;
-    for (int i : h) {
-        sum += height[i];
-    }
-    if (sum == 100) {
-        vector<int> newheight;
-        for (int i : h) {
-            newheight.push_back(height[i]);
-        }
-        sort(newheight.begin(), newheight.end());
-        for (int i : newheight) {
-            cout << i << "\n";
-        }
-        return true;
-    }
-    return false;
+//한줄에 대한거 
+bool print(vector<int>& h){
+	sum =0;
+	for(int i: h) {
+		sum += height[i];
+	}
+	if(sum == 100){
+		vector<int> newheight;
+		for(int i: h) newheight.push_back(height[i]);
+		sort(newheight.begin(), newheight.end());
+		for(int i: newheight) cout << i << "\n";
+		return true;
+	}
+	return false;
 }
 
-bool combi(int start, vector<int>& h) {
-    if (h.size() == 7) {
-        if (print(h)) {
-            return true;
-        }
-        return false;
-    }
-    for (int i = start + 1; i < 9; i++) {
-        h.push_back(i);
-        if (combi(i, h)) {
-            return true;
-        }
-        h.pop_back();
-    }
-    return false;
+bool combi(int start, vector<int>& h){
+	if(h.size()== 7){
+		if(print(h)) return true;
+		return false;
+	}
+	for(int i = start+1; i<9; i++){
+		h.push_back(i);
+		if(combi(i,h)) return true;
+		h.pop_back();
+	}
+	return false;
 }
 
-int main() {
-    for (int i = 0; i < 9; i++) {
-        cin >> height[i];
-    }
-    combi(-1, h);
-    return 0;
+int main(){
+	
+	for(int i=0; i<9; i++){
+		cin >> height[i];
+	}	
+	combi(-1, h);
+	return 0;
 }
