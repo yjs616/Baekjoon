206c60879003d41bd57f69fec2c9c94dd4529103
YU JEONG SEON <yjs000616@gmail.com>
2023-08-04 17:34:55 +0900

    [Bronze II] Title: 8진수 2진수, Time: 36 ms, Memory: 5416 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.cc
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.cc
@@ -1,46 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string convertToBinary(int num) {
-    string binaryStr = "";
-
-    while (num > 0) {
-        int remainder = num % 2;
-        binaryStr = to_string(remainder) + binaryStr;
-        num /= 2;
-    }
-
-    return binaryStr;
+string converToBinary(int num){
+	string binaryStr="";
+	 
+	while(num >0){      //몫이 0이 될 때까지 
+		int remainder = num % 2;
+		binaryStr = to_string(remainder) + binaryStr;
+		num = num/2;
+	}
+	
+	return binaryStr; 
 }
 
-int main() {
-    string octalStr;
-    cin >> octalStr;
-
-    // 입력이 0인 경우 예외 처리
-    if (octalStr == "0") {
-        cout << 0;
-    } else {
-        string binaryStr = "";
-
-        // 각 자리의 8진수 숫자를 2진수로 변환하여 이어붙임
-        for (char digit : octalStr) {
-            int num = digit - '0'; // char를 int로 변환
-            string binaryDigit = convertToBinary(num);
-            // 각 자리의 2진수를 3자리로 맞춰줌
-            while (binaryDigit.size() < 3) {
-                binaryDigit = "0" + binaryDigit;
-            }
-            binaryStr += binaryDigit;
-        }
-
-        // 첫 번째 자리가 0인 경우 0을 제외하고 출력
-        int startIdx = 0;
-        while (binaryStr[startIdx] == '0') {
-            startIdx++;
-        }
-        cout << binaryStr.substr(startIdx);
-    }
-
-    return 0;
+int main(){
+	// 1. 8진수 입력 받기 
+	string octalStr;
+	cin >> octalStr;
+	
+	// 2. string -> int로 
+	if(octalStr == "0") cout << 0;
+	else{
+		string binaryStr="";
+		for(char digit : octalStr){
+			int num = digit-'0';
+			string binaryDigit = converToBinary(num);
+			
+			while(binaryDigit.size()<3){
+				binaryDigit = "0" + binaryDigit;
+			}
+			binaryStr +=binaryDigit;
+		}
+			
+		int startIdx=0;
+		while(binaryStr[startIdx]== '0'){
+			startIdx++;
+		}
+		cout << binaryStr.substr(startIdx);
+	} 
+	
+	return 0;
 }
