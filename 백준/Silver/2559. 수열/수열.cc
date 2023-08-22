e87180871c36f5b36ac335308fa39405592095d4
YU JEONG SEON <yjs000616@gmail.com>
2023-08-22 14:39:26 +0900

    [Silver III] Title: 수열, Time: 28 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n, k;
 
-int main() {
-    int n, k;
-    cin >> n >> k;
-
-    vector<int> tem(n);
-    for (int i = 0; i < n; i++) {
-        cin >> tem[i];
-    }
-
-    int maxSum = 0;
-    int curSum = 0;
-    
-    for (int i = 0; i < k; i++) {
-        curSum += tem[i];
-    }
-    maxSum = curSum;
-
-    for (int i = k; i < n; i++) {
-        curSum = curSum + tem[i] - tem[i - k];
-        maxSum = max(maxSum, curSum);
-    }
-
-    cout << maxSum;
-
-    return 0;
+int main(){
+	
+	cin >> n >> k;
+	
+	vector<int> tem(n);
+	
+	for(int i=0; i<n; i++){
+		cin >> tem[i];
+	}
+	
+	int curSum =0;
+	int maxSum = 0;
+	
+	//초기설정 
+	for(int i=0; i<k; i++){
+		curSum += tem[i];
+	}
+	maxSum = curSum;
+	
+	//슬라이딩 윈도우 
+	for(int i=k; i<n; i++){
+		curSum = curSum + tem[i] - tem[i-k];
+		maxSum = max(curSum, maxSum);
+	}
+	
+	cout << maxSum;
+		
+	return 0;
 }
