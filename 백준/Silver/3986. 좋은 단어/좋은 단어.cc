60300972163996fa539e34f8b9f6919f150f99ad
YU JEONG SEON <yjs000616@gmail.com>
2023-08-25 18:32:36 +0900

    [Silver IV] Title: 좋은 단어, Time: 20 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,32 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-
-    int cnt = 0;
-    for (int i = 0; i < n; i++) {
-        string word;
-        cin >> word;
-
-        stack<char> st;
-        bool goodWord = true;
-
-        for (char c : word) {
-            if (!st.empty() && st.top() == c) {
-                st.pop(); // 같은 문자가 연속되면 pop
-            } else {
-                st.push(c);
-            }
-        }
-
-        if (st.empty()) {
-            cnt++; // 스택이 비어있다면 좋은 단어
-        }
-    }
-
-    cout << cnt << "\n";
-
-    return 0;
+int n, cnt;
+string word;
+bool goodWord = true;
+int main(){
+	
+	cin >> n;
+	for(int i=0; i<n; i++){
+		cin >> word;
+		
+		stack<char> st;
+		
+		for(char c : word){
+			if(!st.empty() && st.top() == c){
+				st.pop();
+			}else{
+				st.push(c);
+			}
+		}
+		
+		if(st.empty()){
+			cnt++;
+		}
+	}
+	
+	cout << cnt;
+	return 0;
 }
