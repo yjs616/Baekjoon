1f3dfca304a04c878ef9cc2b567afd96730c8600
YU JEONG SEON <yjs000616@gmail.com>
2023-08-26 17:11:40 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+ll a, b, c;
+
+ll go(ll a, ll b){
+	if(b==1) return a%c;
+	
+	ll ret = go(a, b/2);
+	ret = (ret * ret)%c;
+	
+	if(b%2) ret =(ret*a)%c;
+	
+	return ret;
+	
+}
+
+int main(){
+	cin >> a >> b >> c;
+	cout << go(a,b);
+	
+	return 0;
+}
