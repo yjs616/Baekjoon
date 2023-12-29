5984bf5f48d4c55f924f59ba735969fcb8a86c36
YU JEONG SEON <yjs000616@gmail.com>
2023-12-30 02:02:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,35 +1,29 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-//백준 1158번 - 요세푸스 문제
 
 int n, k;
-queue<int> cq;
+queue<int> q;
 
 int main() {
-
 	cin >> n >> k;
 
 	for (int i = 1; i <= n; i++) {
-		cq.push(i);
+		q.push(i);                         //1부터 n명 push
 	}
+
 	cout << "<";
-	while (!cq.empty()) {
-		for (int i = 1; i <= k; i++) {
-			if (i == k) {
-				if (cq.size() == 1) {
-					cout << cq.front();
-				}
-				else {
-					cout << cq.front() << ", ";
-				}
-				cq.pop();
-				break;
-			}
-			cq.push(cq.front());    //먼저 뒤에 넣고
-			cq.pop();               //앞에 제거
+	while (q.size() > 1) {
+		for (int i = 1; i <= k - 1; i++) {     //k-1만큼 뒤로 넘기기
+			q.push(q.front());
+			q.pop();
 		}
+		cout << q.front() <<", ";
+		q.pop();
 	}
-	cout << ">";
+
+	//1개 남았을 때
+	cout << q.front() << ">";
+
 	return 0;
 }
