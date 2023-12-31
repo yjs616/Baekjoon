2e1f0f1a6c0717f613b8f13d19bf9ec237b09226
YU JEONG SEON <yjs000616@gmail.com>
2023-12-31 16:18:04 +0900

    [Silver II] Title: 키로거, Time: 312 ms, Memory: 5876 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.cc
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -17,24 +17,21 @@ int main() {
 		cin >> s;
 
 		for (int i = 0; i < s.size(); i++) {
-			if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' || s[i] >= '0' && s[i] <= '9') {
-				password.push_back(s[i]);
-			}
-			else if (s[i] == '<') {
+			if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' || s[i]>='0'&& s[i]<='9') {
+				password.push_back(s[i]);	
+			}else if (s[i] == '<') {
 				if (!password.empty()) {
 					temp.push_back(password.back());
 					password.pop_back();
 				}
-			}
-			else if (s[i] == '>') {
+			}else if (s[i] == '>') {
 				if (!temp.empty()) {
 					password.push_back(temp.back());
 					temp.pop_back();
 				}
-			}
-			else if (s[i] == '-') {
+			}else if (s[i] == '-') {
 				if (!password.empty()) {
-					password.pop_back();
+				    password.pop_back();
 				}
 			}
 		}
