0eb1c794ad7462194347808431bee83bb4b366e6
YU JEONG SEON <yjs000616@gmail.com>
2024-01-09 20:10:41 +0900

    [Silver V] Title: K번째 수, Time: 1056 ms, Memory: 41084 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+long long int arr[5000000];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// N은 입력받을 수의 갯수, K는 정렬했을때 몇 번째 수인지
+	int N, K;	
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + N);
+	
+	cout << arr[K - 1];
+
+	return 0;
+}
