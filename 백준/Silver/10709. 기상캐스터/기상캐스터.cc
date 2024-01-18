9992fa577bae685fc9413ba2f3f85d77df8a1566
YU JEONG SEON <yjs000616@gmail.com>
2024-01-18 22:02:38 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int h, w, visited[103][103],nx;
+char arr[103][103];
+
+void go(){
+
+}
+
+int main(){
+
+    cin >> h >> w;
+
+    memset(visited, -1, sizeof(visited));   //visited -1로 초기화
+
+    for(int i=0; i<h; i++){
+        for(int j=0; j<w; j++){
+            cin >> arr[i][j];
+
+            if(arr[i][j]=='c') visited[i][j]=0;
+
+        }
+    }
+
+    for(int i=0; i<h; i++){
+        for(int j=0; j<w; j++){ //행
+            if(arr[i][j]=='c'){
+                while(true){
+                    nx = j+1;
+                    if(nx > w || arr[i][nx]=='c') break;
+                    visited[i][nx] = visited[i][j]+1;
+                    ++j;
+                }
+            }
+        }
+    }
+
+    for(int i=0; i<h; i++){
+        for(int j=0; j<w; j++){
+            cout << visited[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
