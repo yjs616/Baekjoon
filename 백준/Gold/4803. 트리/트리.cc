3d3c91b6835b6ca6735f016a773cc3eca9c073c3
YU JEONG SEON <yjs000616@gmail.com>
2024-01-31 23:34:36 +0900

    [Gold IV] Title: 트리, Time: 100 ms, Memory: 2956 KB -BaekjoonHub

---
+++ 백준/Gold/4803. 트리/트리.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> adj[503];
+bool visited[503];
+bool isTree;
+
+void dfs(int cur, int parent) {
+    visited[cur] = true;
+
+    for (int next : adj[cur]) {
+        if (!visited[next]) {
+            dfs(next, cur);
+        } else if (next != parent) {
+            isTree = false;
+        }
+    }
+}
+
+int main() {
+    int caseCnt = 1;
+
+    while (true) {
+        int n, m;
+        cin >> n >> m;
+
+        if (n == 0 && m == 0) {
+            break;
+        }
+
+        // 초기화
+        for (int i = 1; i <= n; i++) {
+            adj[i].clear();
+            visited[i] = false;
+        }
+
+        isTree = true;
+
+        // 그래프 입력 받기
+        for (int i = 0; i < m; i++) {
+            int a, b;
+            cin >> a >> b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+
+        int treeCnt = 0;
+
+        // 모든 정점에 대해 DFS 수행
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfs(i, -1);
+                if (isTree) {
+                    treeCnt++;
+                }
+                isTree = true;
+            }
+        }
+
+        // 결과 출력
+        cout << "Case " << caseCnt << ": ";
+        if (treeCnt == 0) {
+            cout << "No trees." << "\n";
+        } else if (treeCnt == 1) {
+            cout << "There is one tree." << "\n";
+        } else {
+            cout << "A forest of " << treeCnt << " trees." << "\n";
+        }
+
+        caseCnt++;
+    }
+
+    return 0;
+}
