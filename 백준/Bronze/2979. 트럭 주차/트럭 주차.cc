d8054eea1f49fa9e6c3fc26368e5a881eb1b80b0
YU JEONG SEON <yjs000616@gmail.com>
2024-05-13 22:53:59 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,53 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int a,b,c;
-pair<int, int> truck1, truck2, truck3;
-int maxTime, totalTime, subTime;
+int times[3], sum;
+
 int main(){
-	cin >> a >> b >> c;
-	
-	cin >> truck1.first >> truck1.second;
-	cin >> truck2.first >> truck2.second;
-	cin >> truck3.first >> truck3.second;
-	
-	if(truck1.second < truck2.second){
-		maxTime = truck2.second;
-		if(maxTime < truck3.second){
-			maxTime = truck3.second;
-		}
-	}else{
-		maxTime = truck1.second;
-		if(maxTime < truck3.second){
-			maxTime = truck3.second;
-		}
-	}
-	
-	for(int time =1; time<= maxTime; time++){
-		int cnt=0; 
-		subTime=0;
-		
-		if(time >= truck1.first && time < truck1.second){
-			cnt++;
-		}
-		if(time >= truck2.first && time < truck2.second){
-			cnt++;
-		}
-		if(time >= truck3.first && time < truck3.second){
-			cnt++;
-		}
-		
-		if(cnt ==1){
-			subTime = a;
-		}else if(cnt==2){
-			subTime = b*2;
-		}else if(cnt ==3){
-			subTime = c*3;
-		}
-		totalTime += subTime;
-	}
-	
-	cout << totalTime;
-	
-	return 0;
+
+    for(int i=0; i<3; i++){
+        cin >> times[i];
+    }
+
+    pair<int, int> p[3];
+    for(int i=0; i<3; i++){
+        cin >> p[i].first >> p[i].second;
+    }
+
+    //시간 돌리면서 주차요금 합 구하기
+    for(int i=1; i<=100; i++){
+        int cnt=0;
+
+        if(p[0].first < i && p[0].second >=i) cnt++;
+        if(p[1].first < i && p[1].second>=i) cnt++;
+        if(p[2].first < i && p[2].second>=i) cnt++;
+
+        sum += cnt*times[cnt-1];
+
+    }
+
+
+    cout << sum <<"\n";
+
+    return 0;
 }
