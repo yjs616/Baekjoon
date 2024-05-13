08a57b9db9b5f1c0074a88371acfee5f853027a7
YU JEONG SEON <yjs000616@gmail.com>
2024-05-13 22:23:55 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,26 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
 string s;
-int engNum;
-map <char,int> mp;
+int alpabet[27];
 
 int main(){
-	cin >> s;
-	for(int i=0; i<s.size(); i++){
-		mp[s[i]]++;
-	}
-	
-	for(int i=97; i<= 122; i++){
-		for(auto it : mp){
-			if(char(i) ==it.first){
-				engNum = it.second;
-				break;
-			}else{
-				engNum =0;
-			}
-		}
-		cout << engNum << " ";
-	}
-	
-	return 0;
+
+    cin >> s;
+    for(int i=0; i<s.size(); i++){
+        alpabet[s[i]-'a'] +=1;
+    }
+
+    for(int i=0; i<26; i++){
+        cout << alpabet[i] << " ";
+    }
+
+    return 0;
 }
