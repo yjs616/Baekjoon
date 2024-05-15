cb2ba72d7e305323c5973ac3713b9a6211834df9
YU JEONG SEON <yjs000616@gmail.com>
2024-05-15 17:34:22 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,30 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        int clothNum;
-        cin >> clothNum;
-        
-        map<string, int> closet;
-
-        for (int j = 0; j < clothNum; j++) {
-            string clothName, clothType;
-            cin >> clothName >> clothType;
-            closet[clothType]++;
-        }
-
-        int sum = 1;  // 모든 의상을 입지 않는 경우를 고려해서 1로 초기화
-
-        for (auto it : closet) {
-            sum *= (it.second + 1);  // 각 의상 종류별로 선택 가능한 개수를 계산하여 곱해줌
-        }
-        
-        cout << sum - 1 << "\n"; // 모든 의상을 입지 않는 경우 1을 뺌 
-    }
-
-    return 0;
+int n, clothNum;
+string clothName, clothType;
+int main(){
+	cin >> n;
+	
+	for(int i=0; i<n; i++){
+		map<string, int> closet;
+		
+		cin >> clothNum;
+		
+		for(int j=0; j<clothNum; j++){
+			cin >> clothName >> clothType;
+			closet[clothType]++;
+		}
+		
+		long long sum =1;
+		for(auto it : closet){
+			sum *= (it.second+1);
+	    }
+		
+		cout << sum-1 << "\n";	
+	}
+		
+	return 0;
 }
