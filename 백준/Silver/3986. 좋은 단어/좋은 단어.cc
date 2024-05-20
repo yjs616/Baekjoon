b4dc12eb416ac3852f3c7de4c294862530e7e263
YU JEONG SEON <yjs000616@gmail.com>
2024-05-20 21:35:08 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,29 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
 using namespace std;
+
 int n, cnt;
-string word;
-bool goodWord = true;
+
 int main(){
-	
-	cin >> n;
-	for(int i=0; i<n; i++){
-		cin >> word;
-		
-		stack<char> st;
-		
-		for(char c : word){
-			if(!st.empty() && st.top() == c){
-				st.pop();
-			}else{
-				st.push(c);
-			}
-		}
-		
-		if(st.empty()){
-			cnt++;
-		}
-	}
-	
-	cout << cnt;
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        string s;
+        cin >> s;  //단어 받기
+
+        stack<char> st;
+        for(int j=0; j <s.size(); j++){
+
+            if(st.empty() || st.top() != s[j]){
+                st.push(s[j]);
+            }else{
+                st.pop();
+            }
+        }
+
+        if(st.empty()) cnt++;
+    }
+
+    cout << cnt << "\n";
+
+    return 0;
 }
