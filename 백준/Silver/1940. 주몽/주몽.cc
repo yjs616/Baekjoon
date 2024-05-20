1eba6671652c806c8d9b132c0ce152a9e9036cb0
YU JEONG SEON <yjs000616@gmail.com>
2024-05-20 21:15:50 +0900

    [Silver IV] Title: 주몽, Time: 8 ms, Memory: 2684 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,33 +1,28 @@
 #include <iostream>
 #include <map>
-#include <vector>
 using namespace std;
 
-int n, m, cnt;
+int n, m, a, cnt;
 map<int, int> mp;
-vector<int> keys;
 
 int main(){
-
     cin >> n >> m;
 
     for(int i=0; i<n; i++){
-        int a;
         cin >> a;
-        mp[a]++; 
-        keys.push_back(a);
+        mp[a]++;
     }
-    
 
-    for(int key : keys){
-        if(mp.find(key) != mp.end() && mp.find(m-key) != mp.end()){
-            if(key != m-key || mp[key] >1){
-                cnt++;
-                mp.erase(key);
-                mp.erase(m-key);
-            }
+    for(auto it : mp){
+        int comp = m- it.first;
+
+        if(mp.find(comp)!= mp.end() && it.first < comp){
+            cnt++;
         }
     }
+
     cout << cnt << "\n";
+
+
     return 0;
 }
