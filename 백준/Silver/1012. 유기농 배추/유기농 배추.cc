711c2fb7d7df20ccfc240b8bca88807af75a9e12
YU JEONG SEON <yjs000616@gmail.com>
2024-05-23 19:06:51 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -3,67 +3,73 @@
 #include <tuple>
 #include <string.h>
 using namespace std;
+//지렁이 상하좌우로 이동 
+//총 몇마리의 지렁이가 필요한지
+//connected component 문제
 
-int field[53][53], visited[53][53], y, x, ny, nx;
-int t, m, n, k;
-int dy[] = { -1, 0, 1, 0 };
-int dx[] = { 0, 1, 0, -1 };
+int T, m, n, k;
+int arr[52][52], visited[52][52];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+int y, x, ny, nx;
 
-void bfs(int y, int x) {
-	visited[y][x] = 1;
+void bfs(int y, int x){
+    visited[y][x] = 1;
 
-	queue<pair<int, int>> q;
+    queue<pair<int, int>> q;
+    q.push({y,x});
 
-	q.push({ y,x });
+    while(!q.empty()){
+        tie(y, x) = q.front(); q.pop();
 
-	while (q.size()) {
-		tie(y, x) = q.front(); q.pop();
+        for(int i=0; i<4; i++){
+            ny = y+ dy[i];
+            nx = x + dx[i];
 
-		for (int i = 0; i < 4; i++) {
-			ny = y + dy[i];
-			nx = x + dx[i];
+            if(ny < 0 || ny >=n || nx<0 || nx>=m || !arr[ny][nx]) continue;
+            if(!visited[ny][nx]){
+                visited[ny][nx] = visited[y][x] + 1;
+                q.push({ny, nx});
+            }
+        }
+    }
 
-			if (ny<0 || ny>=n || nx<0 || nx>=m || field[ny][nx] == 0) continue;
-			if (!visited[ny][nx]) {
-				visited[ny][nx] = 1;
-				q.push({ ny, nx });
-			}
-		}	
-	}
+    return;
 }
 
-int main() {
+int main(){
 
-	cin >> t;
+    cin >> T;
+    
+    //각 test에 필요한 배추 지렁이 출력
+    for(int t=0; t<T; t++){
+        cin >> m >> n >> k;
+        int cnt=0;
 
-	for (int i = 0; i < t; i++) {
+        //arr, visited 초기화
+        
+        memset(arr, 0, sizeof(arr));
+        memset(visited, 0, sizeof(visited));
 
-		int cnt=0;
+        for(int i=0; i<k; i++){
+            int a, b;
+            cin >> a >> b;   //a : x, b : y
+            arr[b][a] = 1;
+        }
 
-		cin >> m >> n >> k;        //m:가로길이, n:세로길이
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(arr[i][j] && !visited[i][j]){
+                    //1이고 방문하지 않았으면 bfs돌리기
+                    bfs(i, j);
+                    cnt++;
+                }
+            }
+        }
 
-		memset(field, 0, sizeof(field));
-		memset(visited, 0, sizeof(visited));
- 
-		for (int i = 0; i < k; i++) {
-			cin >> x >> y;
-			field[y][x] = 1;
-		}
+        cout << cnt << "\n";
 
+    }
 
-		for (int i = 0; i < n; i++) { //y
-			for (int j = 0; j < m; j++) { //x
-				if (field[i][j] == 1 && visited[i][j] == 0) {
-					bfs(i, j);
-					cnt++;
-				}
-			}
-
-		}
-
-		cout << cnt << "\n";
-	}
-
-	
-	return 0;
+    return 0;
 }
