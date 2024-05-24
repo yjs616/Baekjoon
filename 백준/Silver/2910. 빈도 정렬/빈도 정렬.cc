9166a01091c97360b941428a7488e6efa19bbc8a
YU JEONG SEON <yjs000616@gmail.com>
2024-05-24 12:20:00 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <vector>
-#include <tuple>
 #include <algorithm>
 using namespace std;
 
-int n,c;
-map<int, int> mp;      //1순위 : counting
-map<int, int> mp_first;   //2순위 : index
+//메시지 n개로 이루어진 수열, 모두 C보다 작음
+//자주 등장하는 빈도순대로 정렬
+//자주 등장하는 것이 더 앞에 나와야 하고,
+//횟수가 같다면 먼저 나온 것이 앞에   (VV)
+
+int N, C;
+map<int, int> mp;     //1순위 : counting
+map<int, int> mp_first;  //2순위 : index
 vector<pair<int, int>> v;
 
-bool cmp(pair<int, int> a, pair<int, int> b){
+bool cmp(pair<int, int>a, pair<int, int> b){
     if(a.first == b.first){
         return mp_first[a.second] < mp_first[b.second];
     }
@@ -18,31 +23,28 @@ bool cmp(pair<int, int> a, pair<int, int> b){
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-    
-    cin >> n >> c;
+    cin >> N >> C;
 
-    int num;
-    int std = 0;
-    for(int i=0; i<n; i++){
-        cin >> num;
-        mp[num]++;     //counting
+    for(int i=0; i<N; i++){
+        int a;
+        cin >> a;
+        mp[a]++;   //counting
 
-        if(mp_first[num]==0) mp_first[num] = i+1;   //index
+        if(mp_first[a]==0) mp_first[a] = i+1;  //index 저장
     }
 
     for(auto it : mp){
-        v.push_back({it.second, it.first});   //it.second : 1순위, it.firts : 2순위
+        v.push_back({it.second, it.first});   //it.second : 1순위(빈도수), it.first : 2순위(인덱스)
     }
 
     sort(v.begin(), v.end(), cmp);
 
     for(auto i : v){
-        for(int j=0; j< i.first; j++){
+        for(int j=0; j<i.first; j++){
             cout << i.second << " ";
         }
     }
+
+
     return 0;
 }
