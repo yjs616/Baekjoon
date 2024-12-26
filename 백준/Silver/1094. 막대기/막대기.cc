97dd68edc27210570554ec04915e136d7d5cf088
YU JEONG SEON <yjs000616@gmail.com>
2024-12-26 20:09:55 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -1,33 +1,12 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-
-int x, cnt=1;
-
-int main(){
-
-    cin >> x;
-
-    int sum = 64;
-    for(int i = 32; i>0; (i >>= 1)){
-
-        if(sum == x) break;
-
-        //---------------------
-
-        //업데이트
-        if(sum > x){
-            sum=i;
-            continue;
-        }else if(sum < x){
-            if(sum + i > x) continue;
-            else sum += i;
-        }
-
-        cnt++;
-
-    }
-
-    cout << cnt << "\n";
+int n, ret = 1;
+int main(){ 
+	cin >> n; 
+	while(n != 1){
+        if(n & 1) ret++; 
+        n /= 2;
+	}
+	cout << ret << '\n';
     return 0;
 }
