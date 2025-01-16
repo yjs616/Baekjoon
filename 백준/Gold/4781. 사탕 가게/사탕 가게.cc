17a06521cd0ff36f8eb1ee4edbaced2749090ec2
YU JEONG SEON <yjs000616@gmail.com>
2025-01-16 23:59:00 +0900

    [Gold IV] Title: 사탕 가게, Time: 192 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.cc
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <cstring>
+using namespace std;
+int cost, n, m1, m2, c; 
+int dp[100004];
+int main(){
+    while(1){ 
+        scanf("%d %d.%d", &n, &m1, &m2);
+        if(n == 0) break;
+        int cost = m1 * 100 + m2; 
+        memset(dp, 0, sizeof(dp));
+        for(int i = 0; i < n; i++){ 
+            scanf("%d %d.%d", &c, &m1, &m2);
+            int p = m1 * 100 + m2;
+            for(int j = p; j <= cost; j++){
+                dp[j] = max(dp[j], dp[j - p] + c);
+            }
+        }
+        printf("%d\n", dp[cost]);
+    }
+    return 0;
+}
