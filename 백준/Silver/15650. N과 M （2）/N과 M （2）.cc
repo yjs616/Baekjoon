f5e06a6cb3a20c908c665a8a52414ac539a1022b
YU JEONG SEON <yjs000616@gmail.com>
2025-01-22 00:02:10 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,41 +1,44 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+//1부터 N까지 자연수 중에서 중복없이 M개를 고른 수열
+//조합 : nCr
 int n, m;
-int arr[10];  // 1부터 N까지의 숫자를 저장할 배열
-bool visited[10];  // 방문 여부를 확인할 배열
+int arr[10];
+int visited[10];
+vector<int> v; 
 
-void solve(int start, int cnt) {
-    if (cnt == m) {
-        for (int i = 1; i <= n; i++) {
-            if (visited[i]) {
-                cout << arr[i] << " ";
-            }
+void solve(int start, int cnt){
+    if(cnt == m){
+        //출력
+        for(int i=1; i<=n; i++){
+            if(visited[i]) cout << arr[i] << " ";
         }
         cout << "\n";
         return;
     }
-
-    // start부터 n까지 숫자 선택 (오름차순 조합)
-    for (int i = start; i <= n; i++) {
-        if (!visited[i]) {
-            visited[i] = true;  // 선택
-            solve(i + 1, cnt + 1);  // 다음 숫자 선택 (중복 방지)
-            visited[i] = false;  // 선택 해제 (백트래킹)
+    
+    //start부터 n까지 숫자선택
+    for(int i=start; i<=n; i++){
+        if(visited[i] ==0){
+            visited[i] = 1;
+            solve(i+1, cnt+1);
+            visited[i] = 0;
         }
     }
 }
 
-int main() {
+int main(){
+
     cin >> n >> m;
 
-    // 1부터 N까지의 자연수 채우기
-    for (int i = 1; i <= n; i++) {
+    for(int i=1; i<=n; i++){
         arr[i] = i;
     }
 
-    // 백트래킹 시작
     solve(1, 0);
 
+    
     return 0;
 }
