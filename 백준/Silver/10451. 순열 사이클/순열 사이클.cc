9359c9b38cd842c4c2d26f57c9957e31e1071d0c
YU JEONG SEON <yjs000616@gmail.com>
2025-01-30 15:21:16 +0900

    [Silver III] Title: 순열 사이클, Time: 148 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.cc
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.cc
@@ -1,46 +1,46 @@
 #include <iostream>
-#include <vector>
+#include <cstring>
 using namespace std;
 
-int t, visited[1003];
-int adj[1003];
+int t, n, arr[1003];
+int visited[1003];
 
-void dfs(int u) {
-	visited[u] = 1;
-	if (visited[adj[u]] == 0) {
-		dfs(adj[u]);	
-	}	
+void dfs(int num){
+    visited[num] = 1;
+    /*for(int i : arr[num]){
+        if(!visited[i]) dfs(i);
+    }*/
+    if(!visited[arr[num]]) dfs(arr[num]);
 }
 
-int main() {
+int main(){
 
-	cin >> t;
+    cin >> t;
 
-	for (int i = 0; i < t; i++) {
-		int n;
-		cin >> n;
-		
-		//vertext 연결
-		for (int j = 1; j <= n; j++) { 
-			cin >> adj[j];
-		}
+    while(t--){
+        cin >> n;  //순열 크기
 
-		//cycle과 visited 초기화
-		int cycle = 0;
-		for (int j = 1; j <= n; j++) {
-			visited[j] = false;
-		}
+        //초기화
+        int cnt = 0;
+        memset(visited, 0, sizeof(visited));
 
-		for (int k = 1; k <= n; k++) {
-			if (visited[k] == 0) {
-				dfs(k);
-				cycle++;    //재귀 끝나면 cycle++
-			}
-		}	
+        for(int s=1; s<=n; s++){
+            int e;
+            cin >> e;
+            arr[s] = e;
+        }
 
-		cout << cycle << "\n";
+        for(int i=1; i<=n; i++){
+            if(!visited[i]){
+                dfs(i);
+                //cout << "실행 " << "\n";
+                cnt++;
+            }
+        }
 
-	}
+        cout << cnt << "\n";
+    }
 
-	return 0;
+
+    return 0;
 }
